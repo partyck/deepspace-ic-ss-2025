034edb5b0499fad8aae2b829f2500c62006052c9
Patrick Cuellar <patrick.cuellar.o@gmail.com>
2025-06-13 16:21:42 +0200

    rave, circle (#9)
    * v1
    
    * bg
    
    * camera fix + rave

--- deepspace_ss_2025/SceneRave.java
+++ deepspace_ss_2025/SceneRave.java
@@ -12,31 +12,19 @@ import processing.core.PImage;
 import processing.video.*;
 import TUIO.*;
 
-public class SceneCamera extends AbstractScene {
-    private Capture cam;
+public class SceneRave extends AbstractScene {
     private TuioClient tracker;
     private int noiseDetail = 4;
 
     private ArrayList<Dancer> dancers;
     private ArrayList<Dancer> hull;
-    private LinkedList<int[]> buffer;
 
     private NoiseGrid grid;
     private float alphaFade = 3.0f;
 
     boolean showWallGrid = true;
 
-    float noiseScale = 0.001f;
-    float baseNoiseAmount = 10;
-    float influenceRadius = 10;
-    float maxPush = 300;
-    float lerpAmount = 0.05f;
-    float[][] offsets;
-    int circles = 8;
-    int points = 32;
-    int separation = 50;
-
-    public SceneCamera(PApplet p, Capture cam, TuioClient tracker) {
+    public SceneRave(PApplet p, TuioClient tracker) {
         super(p);
         this.tracker  = tracker;
         grid = NoiseGrid.getInstance();
@@ -46,8 +34,6 @@ public class SceneCamera extends AbstractScene {
             grid.setFloor(this);
         } else {
             grid.setWall(this);
-            this.cam = cam;
-            buffer = new LinkedList<>();
         }
         grid.init();
     }
@@ -55,46 +41,14 @@ public class SceneCamera extends AbstractScene {
     @Override
     public void init() {
         noiseDetail(noiseDetail);
-        circles = floor(width() / separation);
-        updateGrid();
-        background(0);
     }
 
     @Override
     public void drawWall() {
-        // fill(0, alphaFade);
-        // noStroke();
-        // rect(0, 0, width(), height());
-        background(0);
-
-        if (cam.available()) {
-            cam.read();
-            cam.filter(12);
-            cam.loadPixels();
-            cam.updatePixels();
-        }
-        PImage frame = cam.get();
-        frame.filter(12);
-        float aspectRatio = (float) frame.height / (float) frame.width;
-
-        buffer.add(frame.pixels);
-        if (buffer.size() > 300) {
-            int[] bufferFrame = buffer.remove();
-            int[] bufferFrame1 = buffer.get(100);
-            int[] bufferFrame2 = buffer.get(200);
-            PImage bufferImage = new PImage(frame.width, frame.height, bufferFrame, false, p);
-            PImage bufferImage1 = new PImage(frame.width, frame.height, bufferFrame1, false, p);
-            PImage bufferImage2 = new PImage(frame.width, frame.height, bufferFrame2, false, p);
-            // image(bufferImage, 0, 0, width() * 0.25f, width() * 0.25f * aspectRatio);
-            // image(bufferImage1, width() - (width() * 0.25f), 0, width() * 0.25f, width() * 0.25f * aspectRatio);
-            // image(bufferImage2, width() - (width() * 0.25f), height() - width() * 0.25f * aspectRatio, width() * 0.25f, width() * 0.25f * aspectRatio);
-            image(bufferImage, 0, 0);
-            image(bufferImage1, width() - (width() * 0.25f), 0);
-            image(bufferImage2, width() - (width() * 0.25f), height() - width() * 0.25f * aspectRatio);
-        }
-
-        image(cam, 0, 0, 0, 0);
-        image(frame, width() * 0.25f, (height() - width() * 0.5f * aspectRatio) * 0.5f, width() * 0.5f, width() * 0.5f * aspectRatio);
+        fill(0, alphaFade);
+        noStroke();
+        rect(0, 0, width(), height());
+        grid.displayWall();
 
         System.out.println("wall frameRate: "+frameRate());
     }
@@ -110,14 +64,9 @@ public class SceneCamera extends AbstractScene {
         for (Dancer dancer : dancers) {
             grid.affect(dancer.x, dancer.y);
         }
-
-        stroke(255);
-        strokeWeight(1);
-        noFill();
-        drawCircles();
         
-        // grid.update();
-        // grid.displayFloor();
+        grid.update();
+        grid.displayFloor();
 
         stroke(255, 0, 0);
         strokeWeight(6);
@@ -125,81 +74,6 @@ public class SceneCamera extends AbstractScene {
             point(p.x, p.y);
         }
         // System.out.println("floor frameRate: "+frameRate());
-    }
-
-    private void drawCircles() {
-        float centerX = width() / 2;
-        float centerY = height() / 2;
-        float zoff = frameCount() * 0.01f;
-        ArrayList<TuioCursor> tuioCursorList = tracker.getTuioCursorList();
-
-        for (int circleI = 0 ; circleI < circles ; circleI++) {
-            int noiseRadius = (circleI + 1) * separation;
-            beginShape();
-            for (int angleI = 0; angleI < points; angleI++) {
-                float radius = offsets[circleI][angleI];
-                float angle = map(angleI, 0, points, 0, PConstants.TWO_PI);
-                int x = (int) (centerX + cos(angle) * radius);
-                int y = (int) (centerY + sin(angle) * radius);
-                int xn = (int) (centerX + cos(angle) * noiseRadius);
-                int yn = (int) (centerY + sin(angle) * noiseRadius);
-
-                float n = noise(xn * noiseScale, yn * noiseScale, zoff);
-                float baseWave = map(n, 0, 1, -baseNoiseAmount, baseNoiseAmount);
-                float influence = 0;
-                
-                for(TuioCursor cursor: tuioCursorList) {
-                    int px = cursor.getScreenX(width());
-                    int py = cursor.getScreenY(height());
-                    float dx = x - px;
-                    float dy = y - py;
-                    float d = dist(x, y, px, py);
-
-                    if (d < influenceRadius) {
-                        float strength = 1 - (d / influenceRadius);
-                        strength *= strength;
-                        float dRadius = dist(centerX, centerY, px, py);
-                        influence = (radius - dRadius) * strength * maxPush;
-                    }
-                }
-                float target = radius + baseWave + influence;
-                offsets[circleI][angleI] = lerp(radius, target, lerpAmount);
-
-                x = (int) (centerX + cos(angle) * offsets[circleI][angleI]);
-                y = (int) (centerY + sin(angle) * offsets[circleI][angleI]);
-                curveVertex(x, y);
-            }
-
-            for (int angleI = 0; angleI < 3; angleI++) {
-                float angle = map(angleI, 0, points, 0, PConstants.TWO_PI);
-                int x = (int) (centerX + cos(angle) * offsets[circleI][angleI]);
-                int y = (int) (centerY + sin(angle) * offsets[circleI][angleI]);
-                curveVertex(x, y);
-            }
-            endShape(PConstants.CLOSE);
-        }
-
-        // for (int circleI = 0; circleI < circles; circleI++) {
-        //     for (int angleI = 0; angleI < points; angleI++) {
-        //         float radius = offsets[circleI][angleI];
-        //         float angle = map(angleI, 0, points, 0, PConstants.TWO_PI);
-        //         int x = (int) (centerX + cos(angle) * radius);
-        //         int y = (int) (centerY + sin(angle) * radius);
-        //         stroke(255, 0, 0);
-        //         strokeWeight(6);
-        //         point(x, y);
-        //     }
-        // }
-    }
-
-    void updateGrid() {
-        offsets = new float[circles][points];
-        for (int circleI = 0; circleI < circles; circleI++) {
-            int radius = (circleI + 1) * separation;
-            for (int angleI = 0; angleI < points; angleI++) {
-                offsets[circleI][angleI] = radius;
-            }
-        }
     }
 
     @Override
@@ -258,19 +132,19 @@ public class SceneCamera extends AbstractScene {
         switch(path) {
             case "/cam/fader2":
                 NoiseGrid.speed = value;
-                System.out.println("    grid.speed: "+NoiseGrid.speed);
+                System.out.println("    NoiseGrid.speed: "+NoiseGrid.speed);
                 break;
             case "/cam/fader3":
-                noiseScale = map(value, 0, 1, 0.0001f, 0.1f);
-                System.out.println("    noiseScale: "+noiseScale);
+                NoiseGrid.noiseScale = map(value, 0, 1, 0.0001f, 0.1f);
+                System.out.println("    NoiseGrid.noiseScale: "+NoiseGrid.noiseScale);
                 break;
             case "/cam/fader4":
                 NoiseGrid.speedFill = map(value, 0, 1, 0.000001f, 0.1f);
-                System.out.println("    grid.speedFill: "+NoiseGrid.speedFill);
+                System.out.println("    NoiseGrid.speedFill: "+NoiseGrid.speedFill);
                 break;
             case "/cam/fader5":
                 NoiseGrid.noiseScaleFill = value;
-                System.out.println("    grid.noiseScaleFill: "+NoiseGrid.noiseScaleFill);
+                System.out.println("    NoiseGrid.noiseScaleFill: "+NoiseGrid.noiseScaleFill);
                 break;
             case "/cam/fader6":
                 noiseDetail = floor(map(value, 0, 1, 1, 8));
@@ -279,7 +153,7 @@ public class SceneCamera extends AbstractScene {
                 break;
             case "/cam/fader7":
                 NoiseGrid.noiseLinesForceStrength = value;
-                System.out.println("    grid.noiseLinesForceStrength: "+NoiseGrid.noiseLinesForceStrength);
+                System.out.println("    NoiseGrid.noiseLinesForceStrength: "+NoiseGrid.noiseLinesForceStrength);
                 break;
             case "/cam/fader8":
                 alphaFade = 255 * value;
@@ -352,7 +226,7 @@ public class SceneCamera extends AbstractScene {
                     }
                 }
                 if (toAdd) {
-                    dancers.add(new Dancer(this, cursor));
+                    dancers.add(new Dancer(cursor));
                 }
             }
         }
@@ -422,13 +296,11 @@ public class SceneCamera extends AbstractScene {
         float x;
         float y;
         long cursorId;
-        SceneCamera scene;
         
-        Dancer(SceneCamera scene, TuioCursor cursor) {
-            this.scene = scene;
+        Dancer(TuioCursor cursor) {
             this.cursorId = cursor.getSessionID();
-            x = cursor.getScreenX(scene.width()); 
-            y = cursor.getScreenY(scene.height());
+            x = cursor.getScreenX(width()); 
+            y = cursor.getScreenY(height());
         }
 
         boolean isLinkedTo(TuioCursor cursor) {
@@ -436,8 +308,6 @@ public class SceneCamera extends AbstractScene {
         }
 
         void update(TuioCursor cursor) {
-            // x = cursor.getScreenX(Constants.WIDTH) - width() / 2f;
-            // y = cursor.getScreenY(Constants.FLOOR_HEIGHT) - height() / 2f;
             x = cursor.getScreenX(width());
             y = cursor.getScreenY(height());
         }
